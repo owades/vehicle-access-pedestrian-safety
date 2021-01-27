99fa70edc0d3189f01413d8658f5d71c644205de
Scott Owades <jeremyscottowades@gmail.com>
2021-01-26 21:04:30 -0800

    script now creates two maps

--- census_data.py
+++ census_data.py
@@ -89,24 +89,24 @@ census_tracts = tracts.merge(census_data, left_on='tract_num', right_on='tract_n
 # tracts.to_csv('tracts.csv',index=False,header=True)
 # census_tracts.to_csv('census_tracts.csv',index=False,header=True)
 
-####### Import & process accident data #######
+####### Import & process crash data #######
 
 # CHP SWITRS data: https://iswitrs.chp.ca.gov/Reports/jsp/CollisionReports.jsp
 # more info about SWITRS program: https://www.chp.ca.gov/programs-services/services-information/switrs-internet-statewide-integrated-traffic-records-system
 
-firsthalf_accidents = pandas.read_csv('CollisionRecords_firsthalf_19.csv', low_memory=False)
-secondhalf_accidents = pandas.read_csv('CollisionRecords_secondhalf_19.csv', low_memory=False)
+firsthalf_crashes = pandas.read_csv('CollisionRecords_firsthalf_19.csv', low_memory=False)
+secondhalf_crashes = pandas.read_csv('CollisionRecords_secondhalf_19.csv', low_memory=False)
 
 # combine firsthalf and secondhalf:
-accidents_2019 = pandas.concat([firsthalf_accidents,secondhalf_accidents]).drop_duplicates().reset_index(drop=True)
+crashes_2019 = pandas.concat([firsthalf_crashes,secondhalf_crashes]).drop_duplicates().reset_index(drop=True)
 
-accidents_2019 = accidents_2019[accidents_2019['LATITUDE'].notna()]
-accidents_2019 = accidents_2019[accidents_2019['LONGITUDE'].notna()]
+crashes_2019 = crashes_2019[crashes_2019['LATITUDE'].notna()]
+crashes_2019 = crashes_2019[crashes_2019['LONGITUDE'].notna()]
 
 # for some reason the CHP data had positive longitude values. Thankfully they only have data for one hemisphere
-accidents_2019['LONGITUDE'] *= -1
+crashes_2019['LONGITUDE'] *= -1
 
-accidents_2019 = accidents_2019[[
+crashes_2019 = crashes_2019[[
 								'CASE_ID',
 								'ACCIDENT_YEAR',
 								'PROC_DATE',
@@ -120,8 +120,8 @@ accidents_2019 = accidents_2019[[
 								'LONGITUDE'
 								]]
 
-# print (accidents_2019.dtypes)
-accidents_2019 = accidents_2019.astype({
+# print (crashes_2019.dtypes)
+crashes_2019 = crashes_2019.astype({
                 	'CASE_ID': int,
 					'ACCIDENT_YEAR': int,
 					'PROC_DATE': int,
@@ -136,59 +136,103 @@ accidents_2019 = accidents_2019.astype({
                	})
 
 # remove data from 2020:
-accidents_2019 = accidents_2019.loc[accidents_2019['COLLISION_DATE'] < 20200101]
+crashes_2019 = crashes_2019.loc[crashes_2019['COLLISION_DATE'] < 20200101]
 
-accidents_2019_geo = geopandas.GeoDataFrame(
-    accidents_2019, geometry=geopandas.points_from_xy(accidents_2019['LONGITUDE'], accidents_2019['LATITUDE']), crs="EPSG:4326")
+crashes_2019_geo = geopandas.GeoDataFrame(
+    crashes_2019, geometry=geopandas.points_from_xy(crashes_2019['LONGITUDE'], crashes_2019['LATITUDE']), crs="EPSG:4326")
 # filter for ped-invovled:
-ped_accidents_2019 = accidents_2019_geo.loc[accidents_2019['COUNT_PED_KILLED'] + accidents_2019['COUNT_PED_INJURED'] > 0]
-# ped_accidents_2019_mini = ped_accidents_2019.head()
+ped_crashes_2019 = crashes_2019_geo.loc[crashes_2019['COUNT_PED_KILLED'] + crashes_2019['COUNT_PED_INJURED'] > 0]
+# ped_crashes_2019_mini = ped_crashes_2019.head()
 
 
-accidents_with_tracts = geopandas.sjoin(ped_accidents_2019, census_tracts, how="inner")
-# accidents_with_census.to_csv('accidents_with_census.csv',index=False,header=True)
+crashes_with_tracts = geopandas.sjoin(ped_crashes_2019, census_tracts, how="inner")
+# crashes_with_census.to_csv('crashes_with_census.csv',index=False,header=True)
 
-tract_accident_counts = accidents_with_tracts['tract_num'].value_counts().rename_axis('tract_num').reset_index(name='accidents')
+tract_crash_counts = crashes_with_tracts['tract_num'].value_counts().rename_axis('tract_num').reset_index(name='crashes')
 
-tract_accident_counts.to_csv('tract_accident_counts.csv',index=False,header=True)
+# tract_crash_counts.to_csv('tract_crash_counts.csv',index=False,header=True)
 
-# column names for data validation:
-# for col in accidents_2019.columns: 
-#     print(col) 
+## Merge crash data into census tract data ##
+census_tract_crashes = census_tracts.merge(tract_crash_counts, left_on='tract_num', right_on='tract_num', how='left')
+census_tract_crashes['ped_crashes_per_1k_households'] = census_tract_crashes['crashes'] * 1000.0 / census_tract_crashes['total_housing_units']
+census_tract_crashes['ped_crashes_per_1k_households'] = census_tract_crashes['ped_crashes_per_1k_households'].fillna(0)
+census_tract_crashes['ped_crashes_per_1k_households'] = census_tract_crashes['ped_crashes_per_1k_households'].fillna(0)
 
+#temporary filter for anomolous value:
+census_tract_crashes = census_tract_crashes.loc[census_tract_crashes['tract_num'] != '4226']
 
-#### Create map of car ownership by tract ####:
+# census_tract_crashes.to_csv('census_tract_crashes.csv',index=False,header=True)
 
-# mapper = linear_cmap(field_name='has_a_vehicle_percent', palette=list(reversed(Cividis256)) ,low=census_data['has_a_vehicle_percent'].min() ,high=census_data['has_a_vehicle_percent'].max())
+### Create map of normalized pedestrian crashes by tract ####:
+print(census_tract_crashes['ped_crashes_per_1k_households'].min())
+print(census_tract_crashes['ped_crashes_per_1k_households'].max())
+mapper = linear_cmap(field_name='ped_crashes_per_1k_households', palette=list(reversed(Cividis256)) ,low=census_tract_crashes['ped_crashes_per_1k_households'].min() ,high=census_tract_crashes['ped_crashes_per_1k_households'].max())
 
-# # use list(reversed()) to flip a pallette:
-# # list(reversed(Blues9))
+# use list(reversed()) to flip a pallcensus_tract_crashesette:
+# list(reversed(Blues9))
 
-# map_options = GMapOptions(lat=37.84, lng=-122.2835, map_type="hybrid", zoom=12)
-# API_KEY = os.getenv('GMAP_API_KEY')
-# p = gmap(API_KEY, map_options,plot_width=1000, plot_height=1000) #creates our map!
+map_options = GMapOptions(lat=37.84, lng=-122.2835, map_type="hybrid", zoom=12)
+API_KEY = os.getenv('GMAP_API_KEY')
+p = gmap(API_KEY, map_options,plot_width=1000, plot_height=1000) #creates our map!
 
-# # p = figure(background_fill_color="white")
-# # the projection is weird...maybe use a tile provider here instead: https://docs.bokeh.org/en/latest/docs/user_guide/geo.html
+# p = figure(background_fill_color="white")
+# the projection is weird...maybe use a tile provider here instead: https://docs.bokeh.org/en/latest/docs/user_guide/geo.html
 
-# p.yaxis.major_tick_line_color = None  # turn off y-axis major ticks
-# p.yaxis.minor_tick_line_color = None  # turn off y-axis minor ticks
-# p.xaxis.major_tick_line_color = None  # turn off y-axis major ticks
-# p.xaxis.minor_tick_line_color = None  # turn off y-axis minor ticks
-# p.xaxis.major_label_text_font_size = '0pt'  # turn off x-axis tick labels
-# p.yaxis.major_label_text_font_size = '0pt'  # turn off y-axis tick labels
+p.yaxis.major_tick_line_color = None  # turn off y-axis major ticks
+p.yaxis.minor_tick_line_color = None  # turn off y-axis minor ticks
+p.xaxis.major_tick_line_color = None  # turn off y-axis major ticks
+p.xaxis.minor_tick_line_color = None  # turn off y-axis minor ticks
+p.xaxis.major_label_text_font_size = '0pt'  # turn off x-axis tick labels
+p.yaxis.major_label_text_font_size = '0pt'  # turn off y-axis tick labels
 
-# geo_source = GeoJSONDataSource(geojson=census_tracts.to_json()) #map will contain our census tract data
+geo_source = GeoJSONDataSource(geojson=census_tract_crashes.to_json()) #map will contain our census tract data
 
-# p.patches('xs', 'ys', fill_color =mapper, line_color='black', fill_alpha=0.8, source=geo_source) # creates "patches" (ie shapes) based on our geo data
-# color_bar = ColorBar(color_mapper=mapper['transform'], width=8,  location=(0,0))
-# p.add_layout(color_bar, 'right')
+p.patches('xs', 'ys', fill_color =mapper, line_color='black', fill_alpha=0.8, source=geo_source) # creates "patches" (ie shapes) based on our geo data
+color_bar = ColorBar(color_mapper=mapper['transform'], width=8,  location=(0,0))
+p.add_layout(color_bar, 'right')
 
-# TOOLTIPS = [
-#     ('Has a vehicle percent', '@has_a_vehicle_percent'),
-#     ('Homes','@total_housing_units')
-# ]
+TOOLTIPS = [
+    ('Tract Number', '@tract_num'),
+	('Ped crashes per k households', '@ped_crashes_per_1k_households'),
+    ('Homes','@total_housing_units')
+]
 
-# p.add_tools( HoverTool(tooltips=TOOLTIPS))
+p.add_tools( HoverTool(tooltips=TOOLTIPS))
 
-# show(p)
+show(p)
+
+## Create map of car ownership by tract ####:
+
+mapper = linear_cmap(field_name='has_a_vehicle_percent', palette=list(reversed(Cividis256)) ,low=census_data['has_a_vehicle_percent'].min() ,high=census_data['has_a_vehicle_percent'].max())
+
+# use list(reversed()) to flip a pallcensus_tract_crashesette:
+# list(reversed(Blues9))
+
+map_options = GMapOptions(lat=37.84, lng=-122.2835, map_type="hybrid", zoom=12)
+API_KEY = os.getenv('GMAP_API_KEY')
+q = gmap(API_KEY, map_options,plot_width=1000, plot_height=1000) #creates our map!
+
+# q = figure(background_fill_color="white")
+# the projection is weird...maybe use a tile provider here instead: https://docs.bokeh.org/en/latest/docs/user_guide/geo.html
+
+q.yaxis.major_tick_line_color = None  # turn off y-axis major ticks
+q.yaxis.minor_tick_line_color = None  # turn off y-axis minor ticks
+q.xaxis.major_tick_line_color = None  # turn off y-axis major ticks
+q.xaxis.minor_tick_line_color = None  # turn off y-axis minor ticks
+q.xaxis.major_label_text_font_size = '0pt'  # turn off x-axis tick labels
+q.yaxis.major_label_text_font_size = '0pt'  # turn off y-axis tick labels
+
+geo_source = GeoJSONDataSource(geojson=census_tracts.to_json()) #map will contain our census tract data
+
+q.patches('xs', 'ys', fill_color =mapper, line_color='black', fill_alpha=0.8, source=geo_source) # creates "patches" (ie shapes) based on our geo data
+color_bar = ColorBar(color_mapper=mapper['transform'], width=8,  location=(0,0))
+q.add_layout(color_bar, 'right')
+
+TOOLTIPS = [
+    ('Has a vehicle percent', '@has_a_vehicle_percent'),
+    ('Homes','@total_housing_units')
+]
+
+q.add_tools( HoverTool(tooltips=TOOLTIPS))
+
+show(q)
